56d637cda69059986fdfea372225cb8268c6bfe0
balani98 <balanideepanshu92@gmail.com>
2020-03-24 23:15:40 +0530

    vidly project started

---
+++ vidly/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vidly.Models
+{
+    public class Movie
+    {    //these are properties of class
+        public int id {get; set;}
+        public string name { get; set; }
+    }
+}
