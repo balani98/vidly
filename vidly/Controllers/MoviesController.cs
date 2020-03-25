f72f2986c00312fd9fcfb30d2b39707fd50f3c24
balani98 <balanideepanshu92@gmail.com>
2020-03-25 21:23:47 +0530

    training completed till action parameters

--- vidly/Controllers/MoviesController.cs
+++ vidly/Controllers/MoviesController.cs
@@ -13,8 +13,27 @@ namespace vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie()
-            { name = "agneepath" };
-            return View();
+            { name = "sanju" };
+            // return View();
+            // return Content("deepanshu balani is Gr8");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home",new {page=1,sortBy="name"});
+        }
+        //PUT:Movies/Edit/:id
+        public ActionResult Edit(int id)
+        {
+            return Content("id="+ id);
+        }
+        //pagination and sorting(pageIndex and sortBy are optional parameters)
+        //:GET:Movies
+        public ActionResult Index(int?pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(string.Format("pageIndex={1}&sortBy={0}", pageIndex, sortBy));
         }
     }
 }
