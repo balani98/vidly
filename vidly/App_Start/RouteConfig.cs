6185b7939aebfe4097f6946a80f26770d009b75b
balani98 <balanideepanshu92@gmail.com>
2020-03-27 10:56:12 +0530

    completed till ViewModels

--- vidly/App_Start/RouteConfig.cs
+++ vidly/App_Start/RouteConfig.cs
@@ -12,8 +12,16 @@ namespace vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
+            //attribute routing
+            routes.MapMvcAttributeRoutes();
+            //routes.MapRoute(
+            //                 "MoviesByReleaseDate",//name of route
+            //                 "movies/released/{year}/{month}",//url
+            //                 new { controller = "movies", action = "ByReleaseDate" },//method and controller name
+            //                 new {year=@"\d{4}",month=@"\d{2}"}//constraints
+            //                 // new { year = @"2015|2016", month = @"\d{2}" }
+            //                 );
+           routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- vidly/Controllers/MoviesController.cs
+++ vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using vidly.Models;
+using vidly.ViewModels;
 
 namespace vidly.Controllers
 {
@@ -14,12 +15,36 @@ namespace vidly.Controllers
         {
             var movie = new Movie()
             { name = "sanju" };
-            // return View();
-            // return Content("deepanshu balani is Gr8");
+            var customers = new List<Customer>
+            {
+                new Customer{name="deepanshu"},
+                new Customer{name="sahil"},
+                new Customer{name="deny"},
+                new Customer{name="philips"},
+                new Customer{name="balani"},
+                new Customer{name="sharma"}
+            
+             };
+            var viewModel = new RandomMovieView_Model()
+            {
+                Movie = movie,
+                Customers = customers
+
+            };
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+            return View(viewModel);
+           // return Content("deepanshu balani is Gr8");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home",new {page=1,sortBy="name"});
+            // return RedirectToAction("Index", "Home",new {page=1,sortBy="name"});
         }
+
+        private ViewResult NewMethod()
+        {
+            return View();
+        }
+
         //PUT:Movies/Edit/:id
         public ActionResult Edit(int id)
         {
@@ -27,13 +52,23 @@ namespace vidly.Controllers
         }
         //pagination and sorting(pageIndex and sortBy are optional parameters)
         //:GET:Movies
+       
         public ActionResult Index(int?pageIndex,string sortBy)
         {
             if (!pageIndex.HasValue)
                 pageIndex = 1;
             if (string.IsNullOrWhiteSpace(sortBy))
                 sortBy = "name";
-            return Content(string.Format("pageIndex={1}&sortBy={0}", pageIndex, sortBy));
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,15)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year + "/" + month);
+        }
+   
+
+
+
     }
 }
