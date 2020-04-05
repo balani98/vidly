6ceaa0b9adb53957d0708fbdf8886fca2ac9100e
balani98 <balanideepanshu92@gmail.com>
2020-04-05 20:16:46 +0530

    Pagination,sorting searching done with jquery Datatables

--- vidly/App_Start/BundleConfig.cs
+++ vidly/App_Start/BundleConfig.cs
@@ -8,8 +8,12 @@ namespace vidly
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                          "~/Scripts/bootbox.js",
+                             "~/Scripts/DataTables/jquery.datatables.js",
+                              "~/Scripts/DataTables/datatables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +24,11 @@ namespace vidly
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                          "~/Scripts/bootbox.js"));
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
